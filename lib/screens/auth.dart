f68bc33aaf388c73ba523dd05a32f8db61fd4b2c
Cinta <cjose@orthofx.com>
2025-06-16 16:50:07 +0530

    StreamBuilder, UI fix - centered scroll view to center the column inside

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -44,91 +44,93 @@ class _AuthScreenState extends State<AuthScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.primary,
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(height: 36,),
-            Image.asset(
-              'assets/images/chat.png',
-              width: 200,
-            ),
-            SizedBox(height: 20,),
-            Container(
-              padding: EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Theme.of(context).colorScheme.onPrimary, 
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: 36,),
+              Image.asset(
+                'assets/images/chat.png',
+                width: 200,
               ),
-              margin: EdgeInsets.all(16),
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  //crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    if (!_isLogin) 
-                      CircleAvatar(radius: 40,),
-                    if (!_isLogin)
-                    TextButton.icon(onPressed: () {}, label: Text('Add Image'), icon: Icon(Icons.photo),),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        labelText: 'Email Address',
-                      ),
-                      keyboardType: TextInputType.emailAddress,
-                      autocorrect: false,
-                      textCapitalization: TextCapitalization.none,
-                      validator: (value) {
-                        if (value == null ||
-                            value.trim().isEmpty ||
-                            !value.contains('@')) {
-                          return 'Please enter a valid email address';
-                        }
-                        return null;
-                      },
-                      onSaved: (value) {
-                        _enteredEmail = value!;
-                      },
-                    ),
-                    if (!_isLogin) 
+              SizedBox(height: 20,),
+              Container(
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Theme.of(context).colorScheme.onPrimary, 
+                ),
+                margin: EdgeInsets.all(16),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    //crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      if (!_isLogin) 
+                        CircleAvatar(radius: 40,),
+                      if (!_isLogin)
+                      TextButton.icon(onPressed: () {}, label: Text('Add Image'), icon: Icon(Icons.photo),),
                       TextFormField(
-                        decoration: const InputDecoration(labelText: 'Username'),
-                        enableSuggestions: false,
+                        decoration: const InputDecoration(
+                          labelText: 'Email Address',
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                        autocorrect: false,
+                        textCapitalization: TextCapitalization.none,
                         validator: (value) {
                           if (value == null ||
                               value.trim().isEmpty ||
-                              value.trim().length < 4) {
-                            return 'Please enter at least 4 characters.';
+                              !value.contains('@')) {
+                            return 'Please enter a valid email address';
                           }
                           return null;
                         },
                         onSaved: (value) {
-                          _enteredUsername = value!;
+                          _enteredEmail = value!;
                         },
                       ),
-                    TextFormField(
-                      decoration: const InputDecoration(labelText: 'Password'),
-                      obscureText: true,
-                      validator: (value) {
-                        if (value == null || value.trim().length < 6) {
-                          return 'Password must be at least 6 characters long.';
-                        }
-                        return null;
-                      },
-                      onSaved: (value) {
-                        _enteredPassword = value!;
-                      },
-                    ),
-                    SizedBox(height: 20,),
-                    ElevatedButton(onPressed: _submit, child: Text('Sign Up'),),
-                    TextButton(onPressed: () {
-                      setState(() {
-                        _isLogin = !_isLogin;
-                      });
-                    }, child: Text(_isLogin ? 'Create a new account' : 'I already have an account')),
-                  ],
+                      if (!_isLogin) 
+                        TextFormField(
+                          decoration: const InputDecoration(labelText: 'Username'),
+                          enableSuggestions: false,
+                          validator: (value) {
+                            if (value == null ||
+                                value.trim().isEmpty ||
+                                value.trim().length < 4) {
+                              return 'Please enter at least 4 characters.';
+                            }
+                            return null;
+                          },
+                          onSaved: (value) {
+                            _enteredUsername = value!;
+                          },
+                        ),
+                      TextFormField(
+                        decoration: const InputDecoration(labelText: 'Password'),
+                        obscureText: true,
+                        validator: (value) {
+                          if (value == null || value.trim().length < 6) {
+                            return 'Password must be at least 6 characters long.';
+                          }
+                          return null;
+                        },
+                        onSaved: (value) {
+                          _enteredPassword = value!;
+                        },
+                      ),
+                      SizedBox(height: 20,),
+                      ElevatedButton(onPressed: _submit, child: Text('Sign Up'),),
+                      TextButton(onPressed: () {
+                        setState(() {
+                          _isLogin = !_isLogin;
+                        });
+                      }, child: Text(_isLogin ? 'Create a new account' : 'I already have an account')),
+                    ],
+                  ),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
